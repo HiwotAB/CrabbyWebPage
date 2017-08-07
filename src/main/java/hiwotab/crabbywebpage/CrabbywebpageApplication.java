d33945bfb31b77145235937733d7c38b65e58a5d
student <behabtuhiwot@gmail.com>
2017-08-07 18:07:31 -0400

    Crabby Cake

---
+++ src/main/java/hiwotab/crabbywebpage/CrabbywebpageApplication.java
@@ -0,0 +1,12 @@
+package hiwotab.crabbywebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabbywebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabbywebpageApplication.class, args);
+	}
+}
